53e0ec355fcd4519c66a8c713f73f5a4df62d94d
kondareddy1229 <kondareddy.devops@gmail.com>
2024-07-06 20:25:43 +0530

    variables is commited

---
+++ 3.variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "PERSON1: Hi $PERSON2, good morning"
+echo "PERSON2: Hi $PERSON1, good morning"
+echo "PERSON1: $PERSON2, how are you"
+echo "PERSON2: $PERSON1, i am fine"
---
+++ 6-variables.sh
@@ -0,0 +1,7 @@
+PERSON1=$1
+PERSON2=$2
+
+echo "PERSON1: Hi $2, good morning"
+echo "PERSON2: Hi $1, good morning"
+echo "PERSON1: $2, how are you"
+echo "PERSON2: $1, i am fine"
