15d34757f310be005b8f1080e936349648f54335
kondareddy1229 <kondareddy.devops@gmail.com>
2024-07-06 22:53:14 +0530

    variables is commited

---
+++ 7-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FRUITS=( "apple" "banana" "mango" )
+echo "First value is:${FRUITS[0]}"
+echo "Second value is:${FRUITS[1]}"
+echo "Third value is:${FRUITS[2]}"
+echo "All fruits is:${FRUITS[@]}"
