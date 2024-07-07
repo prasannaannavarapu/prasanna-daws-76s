d86f676c94e7736acdf4bff05e65c01bf28a1b70
kondareddy1229 <kondareddy.devops@gmail.com>
2024-07-07 17:28:56 +0530

    8 and 9 files

---
+++ 9-install-mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ID=$(id -u)
+if ($ID -ne 0)
+then
+echo "ERROR:please run this script with root access"
+exit1
+else
+echo "you are root user"
+yum install mysql -y
+if($? -ne 0)
+then
+echo "ERROR:Installing mysql is failed"
+exit1
+else
+echo "Installing mysql is success"
+yum install git -y
+if($? -ne 0)
+then
+echo "ERROR:Installing git is failed"
+exit1
+else
+echo "Installing git is success"
