36f3c543851da6f5708a70245f1027330ca919fd
kondareddy1229 <kondareddy.devops@gmail.com>
2024-07-10 17:41:21 +0530

    modified

--- 9-install-mysql.sh
+++ 9-install-mysql.sh
@@ -4,6 +4,7 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
 echo "ERROR:please run this script with root access"
+exit1
 else
 echo "you are root user"
 
@@ -12,6 +13,7 @@ yum install mysql -y
 if [ $? -ne 0 ]
 then
 echo "ERROR:Installing mysql is failed"
+exit1
 else
 echo "Installing mysql is success"
 #yum install git -y
