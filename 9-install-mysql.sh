80dc68691c9280cb2bbbb0f6a8826c29449e2bd2
kondareddy1229 <kondareddy.devops@gmail.com>
2024-07-12 05:23:34 +0530

    9-install-mysql.sh is modified

--- 9-install-mysql.sh
+++ 9-install-mysql.sh
@@ -7,9 +7,7 @@ echo "ERROR:please run this script with root access"
 exit1
 else
 echo "you are root user"
-
-
-yum install mysql -y
+yum install mysql1 -y
 if [ $? -ne 0 ]
 then
 echo "ERROR:Installing mysql is failed"
