b7ba433187df6cdaa87bab08ed35e47f0f762334
kondareddy1229 <kondareddy.devops@gmail.com>
2024-07-11 16:59:56 +0530

    functions commited

---
+++ 10-functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+VALIDATE(){
+    if [ $? -ne 0 ]
+then
+echo "ERROR::Installing is failed"
+exit1
+else
+echo "Installing is success"
+fi
+}
+if [ $? -ne 0 ]
+then
+echo "ERROR::Please run this script with root access"
+exit1
+else
+echo "You are root user"
+fi
+yum install mysql -y
+VALIDATE $? "Installing mysql"
+yum install git -y
+VALIDATE $? "Installing git"
